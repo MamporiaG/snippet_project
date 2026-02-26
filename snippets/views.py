bbe792964ec73938e1b701f69e4dfb2026e91bc6
Guram Mamporia <gurammamphoria@gmail.com>
2026-02-26 16:09:15 +0400

    bug fix

--- snippets/views.py
+++ snippets/views.py
@@ -5,8 +5,8 @@ from .serializers import SnippetSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer()
+    serializer_class = SnippetSerializer
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer()
+    serializer_class = SnippetSerializer
